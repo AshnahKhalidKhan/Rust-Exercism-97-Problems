73512bd7cd2d6bea36791f8ce969617977db6923
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-11 21:08:16 +0500

    Update KindergartenGarden.rs

--- Kindergarten Garden/KindergartenGarden.rs
+++ Kindergarten Garden/KindergartenGarden.rs
@@ -3,37 +3,87 @@ pub fn plants(_diagram: &str, _student: &str) -> Vec<&'static str> {
     /* Grass, Clover, Radishes, Violets */
     let cupLines: Vec<_> = _diagram.lines().collect();
     let mut plants: Vec<char> = Vec::new();
-    println!("{}", _student);
+    // println!("{}", _student);
     for line in cupLines
     {
-        println!("{}", line);
+        // println!("{}", line);
         let cups: Vec<_> = line.chars().collect();
-        println!("{} {}", cups[0], cups[1]);
+        // println!("{} {}", cups[0], cups[1]);
         match _student
         {
             "Alice" =>
                     {
-                        plants.push(cups[0]); //expected `String`, found `char` so use the damn .to_string() function
+                        plants.push(cups[0]);
                         plants.push(cups[1]);
+                    },
+            "Bob" =>
+                    {
+                        plants.push(cups[2]);
+                        plants.push(cups[3]);
+                    },
+            "Charlie" =>
+                    {
+                        plants.push(cups[4]);
+                        plants.push(cups[5]);
+                    },
+            "David" =>
+                    {
+                        plants.push(cups[6]);
+                        plants.push(cups[7]);
+                    },
+            "Eve" =>
+                    {
+                        plants.push(cups[8]);
+                        plants.push(cups[9]);
+                    },
+            "Fred" =>
+                    {
+                        plants.push(cups[10]);
+                        plants.push(cups[11]);
+                    },
+            "Ginny" =>
+                    {
+                        plants.push(cups[12]);
+                        plants.push(cups[13]);
+                    },
+            "Harriet" =>
+                    {
+                        plants.push(cups[14]);
+                        plants.push(cups[15]);
+                    },
+            "Ileana" =>
+                    {
+                        plants.push(cups[16]);
+                        plants.push(cups[17]);
+                    },
+            "Joseph" =>
+                    {
+                        plants.push(cups[18]);
+                        plants.push(cups[19]);
+                    },
+            "Kincaid" =>
+                    {
+                        plants.push(cups[20]);
+                        plants.push(cups[21]);
+                    },
+            "Larry" =>
+                    {
+                        plants.push(cups[22]);
+                        plants.push(cups[23]);
                     },
             _ => println!("Kuch bhi nahin"),
         }
-        // if _student == "Alice"
-        // {
-        //     plants.push(cups[0].to_string());
-        //     plants.push(cups[1].to_string());
-        // }
     }
-    println!("{:?}", plants);
+    // println!("{:?}", plants);
     let mut plantsFullNames: Vec<&str> = Vec::new();
     for plant in plants
     {
         match plant
         {
-            'G' => plantsFullNames.push("grass"),//.to_string()), //expected `String`, found `char` so use the damn .to_string() function
-            'C' => plantsFullNames.push("clover"),//.to_string()),
-            'R' => plantsFullNames.push("radishes"),//.to_string()),
-            'V' => plantsFullNames.push("violets"),//.to_string()),
+            'G' => plantsFullNames.push("grass"),
+            'C' => plantsFullNames.push("clover"),
+            'R' => plantsFullNames.push("radishes"),
+            'V' => plantsFullNames.push("violets"),
             _ => println!("Kuch bhi nahin"),
         }
     }
