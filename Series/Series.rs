2c83f02fc4bb9e497cfe45b731b2364f24310a25
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-12 00:08:48 +0500

    Update Series.rs

--- Series/Series.rs
+++ Series/Series.rs
@@ -4,18 +4,12 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
     let digitsAsCharacters: Vec<char> = digits.chars().collect();
     let mut series: Vec<String> = Vec::new();
     let mut digit: usize = 0;
-    let mut digitsSet: usize = 0;
 
-    println!("digits: {}", digits);
-    println!("digitsAsCharacters: {:?}", digitsAsCharacters);
-    println!("digitsAsCharacters.len(): {}", digitsAsCharacters.len());
-    println!("len: {}", len);
-
-    if digitsAsCharacters.len() < len
+    if digits.len() < len
     {
         return series;
     }
-    else if digitsAsCharacters.len() == len
+    else if digits.len() == len
     {
         series.push(digits.to_string());
         return series;
@@ -25,35 +19,8 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
     while digit <= digits.len()
     {
         let mut seriesElement: String = String::new();
-        while seriesElement.len() < len && (digit + digitsSet) < digits.len()//digitsSet != len && (digit + digitsSet) < digits.len()
-        {
-            seriesElement = seriesElement + &digitsAsCharacters[digit + digitsSet].to_string();
-            digitsSet = digitsSet + 1;
-        }
-        println!("seriesElement: {}", seriesElement);
-        if seriesElement.len() == len //(digit + digitsSet) < digits.len()
-        {
-            series.push(seriesElement);
-        }
-        digitsSet = 0;
-        digit = digit + 1;
-    }
-    series
-    
-    /*
-while digit < digits.len()
-    {
-        // println!("digit < digitsAsCharacters.len(): {} < {}", digit, digitsAsCharacters.len());
-        let mut seriesElement: String = String::new();
-        seriesElement = seriesElement + &digitsAsCharacters[digit + digitsSet].to_string();
-        digitsSet = digitsSet + 1;
-        // while digitsSet < len && (digit + digitsSet) < digitsAsCharacters.len()
-        // {
-        //     println!("digit: {}, digitsSet: {}, digit+digitsSet: {}, ", digit, digitsSet, digit+digitsSet);
-        //     seriesElement = seriesElement + &digitsAsCharacters[digit + digitsSet].to_string();
-        //     digitsSet = digitsSet + 1;
-        // }
-        while digitsSet % len != 0
+        let mut digitsSet: usize = 0;
+        while seriesElement.len() < len && (digit + digitsSet) < digits.len()
         {
             seriesElement = seriesElement + &digitsAsCharacters[digit + digitsSet].to_string();
             digitsSet = digitsSet + 1;
@@ -64,8 +31,6 @@ while digit < digits.len()
             series.push(seriesElement);
         }
         digit = digit + 1;
-        digitsSet = 0;
     }
     series
-*/
 }
