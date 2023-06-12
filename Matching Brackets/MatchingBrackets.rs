27877b038a7730bf98e937ab35ce89234537e0ef
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-12 10:11:42 +0500

    Update MatchingBrackets.rs

--- Matching Brackets/MatchingBrackets.rs
+++ Matching Brackets/MatchingBrackets.rs
@@ -1,7 +1,8 @@
 pub fn brackets_are_balanced(string: &str) -> bool
 {
     //unimplemented!("Check if the string \"{string}\" contains balanced brackets");
-    
+
+    //Remove any characters that are not brackets
     let SeparateCharacters: Vec<char> = string.chars().collect();
     let BracketsPool: Vec<char> = vec!['{', '}', '[', ']', '(', ')'];
     let mut BracketsOnly: Vec<char> = Vec::new();
@@ -13,19 +14,11 @@ pub fn brackets_are_balanced(string: &str) -> bool
         }
     }
     
+    //Find valid bracket pairs and remove them
+    //If you do not find a match for a bracket after traversing the whole array, or if you have removed all bracket pairs and no brackets are left to check, stop checking
     let mut currentbracket: usize = 0;
     while (currentbracket + 1) < BracketsOnly.len()
     {
-        /*if (BracketsOnly[currentbracket] == '{' && BracketsOnly[currentbracket + 1] == '}') || (BracketsOnly[currentbracket] == '[' && BracketsOnly[currentbracket + 1] == ']') || (BracketsOnly[currentbracket] == '(' && BracketsOnly[currentbracket + 1] == ')')
-        {
-            BracketsOnly.remove(currentbracket + 1);
-            BracketsOnly.remove(currentbracket);
-            currentbracket = 0;
-        }
-        else
-        {
-            currentbracket = currentbracket + 1;
-        }*/
         match (BracketsOnly[currentbracket], BracketsOnly[currentbracket + 1])
         {
             ('{', '}') | ('[', ']') | ('(', ')') =>
@@ -37,13 +30,15 @@ pub fn brackets_are_balanced(string: &str) -> bool
             _ => currentbracket = currentbracket + 1
         };
     }
-    
-    if BracketsOnly.len() != 0
+
+    //If there were only valid bracket pairs, then they were all removed and then length of the remaining brackets is zero, which means brackets are balanced so return true
+    //If not, there are brackets left which did not have valid pairs and the brackets are unbalanced so return false
+    if BracketsOnly.len() == 0
     {
-        return false;
+        return true;
     }
     else
     {
-        return true;
+        return false;
     }
 }
