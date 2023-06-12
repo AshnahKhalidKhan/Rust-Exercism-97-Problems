743fcc87ee95da2bffa02cc184b5de9b503bf918
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-12 10:01:22 +0500

    Update MatchingBrackets.rs

--- Matching Brackets/MatchingBrackets.rs
+++ Matching Brackets/MatchingBrackets.rs
@@ -3,10 +3,12 @@ pub fn brackets_are_balanced(string: &str) -> bool
     //unimplemented!("Check if the string \"{string}\" contains balanced brackets");
     
     let SeparateCharacters: Vec<char> = string.chars().collect();
+    let BracketsPool: Vec<char> = vec!['{', '}', '[', ']', '(', ')'];
     let mut BracketsOnly: Vec<char> = Vec::new();
     for c in 0..SeparateCharacters.len()
     {
-        if SeparateCharacters[c] == '{' || SeparateCharacters[c] == '}' || SeparateCharacters[c] == '[' || SeparateCharacters[c] == ']' || SeparateCharacters[c] == '(' || SeparateCharacters[c] == ')'
+        //if SeparateCharacters[c] == '{' || SeparateCharacters[c] == '}' || SeparateCharacters[c] == '[' || SeparateCharacters[c] == ']' || SeparateCharacters[c] == '(' || SeparateCharacters[c] == ')'
+        if BracketsPool.contains(&SeparateCharacters[c])
         {
             BracketsOnly.push(SeparateCharacters[c]);
         }
