6db81d3af3718649125885b7094056cf79424770
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-13 22:16:25 +0500

    Update Bob.rs

--- Bob/Bob.rs
+++ Bob/Bob.rs
@@ -1,44 +1,112 @@
 pub fn reply(message: &str) -> &str {
     //unimplemented!("have Bob reply to the incoming message: {message}")
-    let mut messageBreakdown: Vec<char> = message.chars().collect();
-    println!("{:?}", messageBreakdown);
-    let mut QuestionMarkAtEnd: bool = match messageBreakdown[messageBreakdown.len() - 1]
-                                    {
-                                        '?' => true,
-                                        _ => false,
-                                    };
-    println!("QuestionMarkAtEnd: {}", QuestionMarkAtEnd);
+    // let mut Silence: bool = false;
+    //     let mut messageBreakdown: Vec<char> = message.chars().collect();
+    // println!("{:?}", messageBreakdown);
+    // //Sophisticated answer would be to use this and just be done with it: message.chars().filter(|c| !c.is_whitespace()).collect()
+    // let mut NotWhitespace: Vec<char> = Vec::new();
+    // for index in 0..messageBreakdown.len()
+    // {
+    //     if (messageBreakdown[index] != '\0' && messageBreakdown[index].is_whitespace() == false && messageBreakdown[index].is_ascii_alphabetic() == true) || messageBreakdown[index] == '?'
+    //     {
+    //         NotWhitespace.push(messageBreakdown[index]);
+    //     }
+    // }
 
-    let mut ALLCAPS: bool = true;
-    for character in message.chars()
-    {
-        if character.is_ascii_alphabetic() == true && character != character.to_ascii_uppercase()
-        {
-            ALLCAPS = false;
-            break;
-        }
-    }
-    println!("AllCaps: {}", ALLCAPS);
+    // println!("NotWhiteSpace: {:?}", NotWhitespace);
+    
+    // if NotWhitespace.len() == 0
+    // {
+    //     Silence = true;
+    // }
+    // else
+    // {
+    //     Silence = false;
+    // }
+    // println!("Silence: {}", Silence);
+
+    // let mut QuestionMarkAtEnd: bool = match NotWhitespace[NotWhitespace.len() - 1]
+    //                                 {
+    //                                     '?' => true,
+    //                                     _ => false,
+    //                                 };
+    // println!("QuestionMarkAtEnd: {}", QuestionMarkAtEnd);
+
+    // let mut ALLCAPS: bool = true;
+    // for character in message.chars()
+    // {
+    //     if character.is_ascii_alphabetic() == true && character != character.to_ascii_uppercase()
+    //     {
+    //         ALLCAPS = false;
+    //         break;
+    //     }
+    // }
+    // println!("AllCaps: {}", ALLCAPS);
+    
+    // match (Silence, QuestionMarkAtEnd, ALLCAPS)
+    // {
+    //     (true, _, _) => "Fine. Be that way!",
+    //     (_, true, true) => "Calm down, I know what I'm doing!",
+    //     (_, true, _) => "Sure.",
+    //     (_, _, true) => "Whoa, chill out!",
+    //     _ => "Whatever.",
+    // }
 
     let mut Silence: bool = false;
-    //Sophisticated answer would be to use this and just be done with it: message.chars().filter(|c| !c.is_whitespace()).collect()
-    let mut NotWhitespace: Vec<char> = Vec::new();
+    let mut QuestionMarkAtEnd: bool = false;
+    let mut ALLCAPS: bool = true;
+
+    let mut messageBreakdown: Vec<char> = message.chars().collect();
+    let mut messageBreakdownWithoutWhitespaces: Vec<char> = Vec::new();
+    let mut messageBreakdownWithOnlyAlphabets: Vec<char> = Vec::new();
+
     for index in 0..messageBreakdown.len()
     {
-        if messageBreakdown[messageBreakdown.len() - 1 - index] != '\0' && messageBreakdown[messageBreakdown.len() - 1 - index].is_whitespace() == false
+        if messageBreakdown[index] != '\0' && messageBreakdown[index].is_whitespace() == false
         {
-            NotWhitespace.push(messageBreakdown[messageBreakdown.len() - 1 - index]);
+            messageBreakdownWithoutWhitespaces.push(messageBreakdown[index]);
         }
     }
-    if NotWhitespace.len() == 0
+    if messageBreakdownWithoutWhitespaces.len() == 0
     {
         Silence = true;
     }
-    else
+    else if messageBreakdownWithoutWhitespaces[messageBreakdownWithoutWhitespaces.len() - 1] == '?'
     {
-        Silence = false;
+        QuestionMarkAtEnd = true;
     }
+
+    for index in 0..messageBreakdown.len()
+    {
+        if messageBreakdown[index].is_ascii_alphabetic() == true
+        {
+            messageBreakdownWithOnlyAlphabets.push(messageBreakdown[index]);
+        }
+    }
+    
+    if messageBreakdownWithOnlyAlphabets.len() != 0
+    {
+        for index in 0..messageBreakdownWithOnlyAlphabets.len()
+        {
+            if messageBreakdownWithOnlyAlphabets[index] != messageBreakdownWithOnlyAlphabets[index].to_ascii_uppercase()
+            {
+                ALLCAPS = false;
+                break;
+            }
+        }
+    }
+    else if messageBreakdownWithOnlyAlphabets.len() == 0
+    {
+        ALLCAPS = false;
+    }
+
+    println!("messageBreakdown: {:?}", messageBreakdown);
+    println!("messageBreakdownWithoutWhitespaces: {:?}", messageBreakdownWithoutWhitespaces);
     println!("Silence: {}", Silence);
+    println!("QuestionMarkAtEnd: {}", QuestionMarkAtEnd);
+    println!("messageBreakdownWithOnlyAlphabets: {:?}", messageBreakdownWithOnlyAlphabets);
+    println!("ALLCAPS: {}", ALLCAPS);
+
     match (Silence, QuestionMarkAtEnd, ALLCAPS)
     {
         (true, _, _) => "Fine. Be that way!",
