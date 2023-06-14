009e61efe9793d713bc2c8745c4a88196cc2548c
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-14 12:09:26 +0500

    Update CollatzConjecture.rs

--- Collatz Conjecture/CollatzConjecture.rs
+++ Collatz Conjecture/CollatzConjecture.rs
@@ -1,44 +1,37 @@
-pub fn collatz(n: u64) -> Option<u64> {
+pub fn collatz(num: u64) -> Option<u64>
+{
     //unimplemented!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
-    let mut n: u64 = n;
-    let mut Answer: Option<u64> = Some(0);
-
-    // if n % 2 == 0
-    // {
-    //     n = n/2;
-    // }
-    // else if n % 2 == 1
-    // {
-    //     n = (3*n) + 1;
-    // }
-
+    
+    let mut n: u64 = num;
     let mut times: u64 = 0;
-    while n != 1 && Answer != None
+    let mut Answer: Option<u64> = Some(times);
+
+    while n > 1 && Answer != None
     {
         if n % 2 == 0
         {
-            match n.checked_div(2)
+            Answer = match n.checked_div(2)
             {
                 Some(_) =>
                 {
                     n = n/2;
                     times = times + 1;
-                    Answer = Some(times);
+                    Some(times)
                 },
-                None => Answer = None,
+                None => None,
             }
         }
         else if n % 2 == 1
         {
-            match n.checked_add(n+n+n+1)
+            Answer = match n.checked_add(n+n+n+1)
             {
                 Some(_) =>
                 {
                     n = 3*n + 1;
                     times = times + 1;
-                    Answer = Some(times);
+                    Some(times)
                 },
-                None => Answer = None,
+                None => None,
             }
         }
     }
