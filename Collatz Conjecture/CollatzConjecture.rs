74fefca89ac41ccf69797f6090975e52b8b5565b
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-14 12:42:06 +0500

    Update CollatzConjecture.rs

--- Collatz Conjecture/CollatzConjecture.rs
+++ Collatz Conjecture/CollatzConjecture.rs
@@ -4,33 +4,60 @@ pub fn collatz(num: u64) -> Option<u64>
     
     let mut n: u64 = num;
     let mut times: u64 = 0;
-    let mut Answer: Option<u64> = Some(times);
+    let mut Answer: Option<u64> = match num
+                                {
+                                    0 => None,
+                                    1 => Some(0),
+                                    _ => Some(times),
+                                };
 
     while n > 1 && Answer != None
     {
+        println!("{}", n);
         if n % 2 == 0
         {
             Answer = match n.checked_div(2)
             {
                 Some(_) =>
-                {
-                    n = n/2;
-                    times = times + 1;
-                    Some(times)
-                },
+                        {
+                            n = n/2;
+                            times = times + 1;
+                            Some(times)
+                        },
                 None => None,
             }
         }
         else if n % 2 == 1
         {
-            Answer = match n.checked_add(n+n+n+1)
+            Answer = match n.checked_add(n)
             {
                 Some(_) =>
-                {
-                    n = 3*n + 1;
-                    times = times + 1;
-                    Some(times)
-                },
+                        {
+                            match n.checked_add(n + n)
+                            {
+                                Some(_) =>
+                                        {
+                                            match n.checked_add(n + n + n)
+                                            {
+                                                Some(_) =>
+                                                        {
+                                                            match n.checked_add(n + n + n + 1)
+                                                            {
+                                                                Some(_) =>
+                                                                        {
+                                                                            n = (3*n) + 1;
+                                                                            times = times + 1;
+                                                                            Some(times)
+                                                                        }
+                                                                None => None,
+                                                            }
+                                                        }
+                                                None => None,
+                                            }
+                                        }
+                                None => None,
+                            }
+                        },
                 None => None,
             }
         }
