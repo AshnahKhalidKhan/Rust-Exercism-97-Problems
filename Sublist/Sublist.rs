c4b1e02ac199c36d63e72e6d8300595255e34e03
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-14 11:09:58 +0500

    Update Sublist.rs

--- Sublist/Sublist.rs
+++ Sublist/Sublist.rs
@@ -6,60 +6,98 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+pub fn sublist<T: PartialEq>(A: &[T], B: &[T]) -> Comparison
+{
     //unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
 
     // let mut AllAInB: bool = true;
     // let mut AllBInA: bool = true;
 
+    if A.len() == 0 && B.len() == 0
+    {
+        return Comparison::Equal;
+    }
+    else if A.len() == 0 && B.len() != 0
+    {
+        return Comparison::Sublist;
+    }
+    else if A.len() != 0 && B.len() == 0
+    {
+        return Comparison::Superlist;
+    }
+
+
     let mut pointerInA: usize = 0;
     let mut pointerInB: usize = 0;
     let mut Answer: Comparison = Comparison::Unequal;
-
-    while pointerInA <= pointerInB && pointerInA < _first_list.len() && pointerInB < _second_list.len()
+    
+    if A.len() > B.len()
     {
-        if _first_list[pointerInA] == _second_list[pointerInB]
+        while pointerInA < A.len() && pointerInB < B.len()
         {
+            if B[pointerInB] == A[pointerInA]
+            {
+                pointerInB = pointerInB + 1;
+            }
+            else
+            {
+                while pointerInB > 0
+                {
+                    pointerInB = pointerInB - 1;
+                    if B[pointerInB] == A[pointerInA]
+                    {
+                        pointerInB = pointerInB + 1;
+                        break;
+                    }
+                }
+            }
             pointerInA = pointerInA + 1;
         }
-        else
+    
+        if pointerInB != B.len() //&& pointerInA == A.len()
         {
-            pointerInA = 0;
+            Answer = Comparison::Unequal;
         }
-        pointerInB = pointerInB + 1;
-    }
-
-    if pointerInA != _first_list.len() - 1 && pointerInB == _second_list.len() - 1
-    {
-        Answer = Comparison::Unequal;
-    }
-    else if pointerInA == pointerInB
-    {
-        Answer = Comparison::Equal;
-    }
-    else if pointerInA == _first_list.len() - 1 && _first_list.len() < _second_list.len()
-    {
-        Answer = Comparison::Sublist;
-    }
-
-    pointerInA = 0;
-    pointerInB = 0;
-    while pointerInB <= pointerInA && pointerInA < _first_list.len() && pointerInB < _second_list.len()
-    {
-        if _second_list[pointerInB] == _first_list[pointerInA]
+        else //if pointerInA == A.len() - 1 && A.len() < B.len()
         {
+            Answer = Comparison::Superlist;
+        }
+    }
+    else if A.len() <= B.len()
+    {
+        while pointerInA < A.len() && pointerInB < B.len()
+        {
+            if A[pointerInA] == B[pointerInB]
+            {
+                pointerInA = pointerInA + 1;
+            }
+            else
+            {
+                while pointerInA > 0
+                {
+                    pointerInA = pointerInA - 1;
+                    if A[pointerInA] == B[pointerInB] 
+                    {
+                        pointerInA = pointerInA + 1;
+                        break;
+                    }
+                }
+            }
             pointerInB = pointerInB + 1;
         }
-        else
+    
+        if pointerInA != A.len() && pointerInB == B.len()
         {
-            pointerInB = 0;
+            Answer = Comparison::Unequal;
         }
-        pointerInA = pointerInA + 1;
-    }
-
-    if pointerInB == _second_list.len() - 1 && _second_list.len() < _first_list.len()
-    {
-        Answer = Comparison::Superlist;
+        else if pointerInA == pointerInB && A.len() == B.len()
+        {
+            Answer = Comparison::Equal;
+        }
+        else //if pointerInA == A.len() - 1 && A.len() < B.len()
+        {
+            Answer = Comparison::Sublist;
+        }
     }
 
     Answer
