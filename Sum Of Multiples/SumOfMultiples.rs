256ce9b69f580dc1bc1dd6b25c01c57af160807a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-14 09:39:09 +0500

    Update SumOfMultiples.rs

--- Sum Of Multiples/SumOfMultiples.rs
+++ Sum Of Multiples/SumOfMultiples.rs
@@ -1,21 +1,28 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     //unimplemented!("Sum the multiples of all of {factors:?} which are less than {limit}")
-
+    
     let mut multiples: Vec<u32> = Vec::new();
     println!("Limit: {}", limit);
     println!("Factors: {:?}", factors);
 
     for index in 0..factors.len()
     {
-        let mut finalMultiplier: u32 = limit / factors[index];
-        println!("finalMultiplier: {}", finalMultiplier);
-        // let mut multiplier: u32 = 0;
-        for multiplier in 1..=finalMultiplier
+        if factors[index] == 0
         {
-            if multiples.contains(&(factors[index] * multiplier)) == false && factors[index] * multiplier < limit
+            multiples.push(factors[index]);
+        }
+        else
+        {
+            let mut finalMultiplier: u32 = limit / factors[index];
+            println!("finalMultiplier: {}", finalMultiplier);
+            // let mut multiplier: u32 = 0;
+            for multiplier in 1..=finalMultiplier
             {
-                println!("{} x {} = {}", factors[index], multiplier, factors[index] * multiplier);
-                multiples.push(factors[index] * multiplier);
+                if multiples.contains(&(factors[index] * multiplier)) == false && factors[index] * multiplier < limit
+                {
+                    println!("{} x {} = {}", factors[index], multiplier, factors[index] * multiplier);
+                    multiples.push(factors[index] * multiplier);
+                }
             }
         }
         // let mut multiplier: u32 = 1;
