047ca416cd5fe304753ad7279378a9466757425a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-14 11:28:06 +0500

    Update Sublist.rs

--- Sublist/Sublist.rs
+++ Sublist/Sublist.rs
@@ -14,46 +14,8 @@ pub fn sublist<T: PartialEq>(A: &[T], B: &[T]) -> Comparison
     let mut pointerInB: usize = 0;
     let mut Answer: Comparison = Comparison::Unequal;
     
-    //If A is longer than B, then A is either a superlist or they are unequal.
-    if A.len() > B.len()
-    {
-        //While we have not found all characters in B, or we have not traversed A completely.
-        while pointerInB < B.len() && pointerInA < A.len()
-        {
-            //If the characters at the current pointers in B and A match, check the next character in the pattern B.
-            if B[pointerInB] == A[pointerInA]
-            {
-                pointerInB = pointerInB + 1;
-            }
-            else
-            {
-                //Anchor yourself on the current character in A. Backtrack to the last same character in B. If none match, start all over.
-                while pointerInB > 0
-                {
-                    pointerInB = pointerInB - 1;
-                    if B[pointerInB] == A[pointerInA]
-                    {
-                        pointerInB = pointerInB + 1;
-                        break;
-                    }
-                }
-            }
-            pointerInA = pointerInA + 1;
-        }
-    
-        //If all the characters in B were not traversed, they did not match with a set of consecutive characters in A; they are unequal.
-        if pointerInB != B.len()
-        {
-            Answer = Comparison::Unequal;
-        }
-        //If B was traversed completely, and we already know that A and B do not have same lengths, then A is a superlist of B.
-        else
-        {
-            Answer = Comparison::Superlist;
-        }
-    }
     //If A is shorter or as long as B, then A is either a sublist of B, equal to B, or they are unequal.
-    else if A.len() <= B.len()
+    if A.len() <= B.len()
     {
         //While we have not found all characters in A, or we have not traversed B completely.
         while pointerInA < A.len() && pointerInB < B.len()
@@ -93,6 +55,44 @@ pub fn sublist<T: PartialEq>(A: &[T], B: &[T]) -> Comparison
         else
         {
             Answer = Comparison::Sublist;
+        }
+    }
+    //If A is longer than B, then A is either a superlist or they are unequal.
+    else if A.len() > B.len()
+    {
+        //While we have not found all characters in B, or we have not traversed A completely.
+        while pointerInB < B.len() && pointerInA < A.len()
+        {
+            //If the characters at the current pointers in B and A match, check the next character in the pattern B.
+            if B[pointerInB] == A[pointerInA]
+            {
+                pointerInB = pointerInB + 1;
+            }
+            else
+            {
+                //Anchor yourself on the current character in A. Backtrack to the last same character in B. If none match, start all over.
+                while pointerInB > 0
+                {
+                    pointerInB = pointerInB - 1;
+                    if B[pointerInB] == A[pointerInA]
+                    {
+                        pointerInB = pointerInB + 1;
+                        break;
+                    }
+                }
+            }
+            pointerInA = pointerInA + 1;
+        }
+    
+        //If all the characters in B were not traversed, they did not match with a set of consecutive characters in A; they are unequal.
+        if pointerInB != B.len()
+        {
+            Answer = Comparison::Unequal;
+        }
+        //If B was traversed completely, and we already know that A and B do not have same lengths, then A is a superlist of B.
+        else
+        {
+            Answer = Comparison::Superlist;
         }
     }
 
