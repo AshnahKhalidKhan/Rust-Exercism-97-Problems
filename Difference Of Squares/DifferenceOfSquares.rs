b5e8e779d52226ba1e74aaadc6105101334bb47a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-20 22:37:51 +0500

    Update DifferenceOfSquares.rs

--- Difference Of Squares/DifferenceOfSquares.rs
+++ Difference Of Squares/DifferenceOfSquares.rs
@@ -1,1 +1,29 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    //unimplemented!("square of sum of 1...{n}")
+    let mut number: u32 = 1;
+    let mut sum: u32 = 0;
+    while number <= n
+    {
+        sum = sum + number;
+        number = number + 1;
+    }
+    let mut squareOfSum: u32 = sum*sum;
+    squareOfSum
+}
 
+pub fn sum_of_squares(n: u32) -> u32 {
+    //unimplemented!("sum of squares of 1...{n}")
+    let mut number: u32 = 1;
+    let mut sumOfSquares: u32 = 0;
+    while number <= n
+    {
+        sum = sum + (number*number);
+        number = number + 1;
+    }
+    squareOfSum
+}
+
+pub fn difference(n: u32) -> u32 {
+    //unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
+}
