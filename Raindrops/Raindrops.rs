c01fabcaab3d88625ff9bf0bbcb9146f57d7801d
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-20 22:50:47 +0500

    Update Raindrops.rs

--- Raindrops/Raindrops.rs
+++ Raindrops/Raindrops.rs
@@ -1,21 +1,21 @@
 pub fn raindrops(n: u32) -> String {
     //unimplemented!("what sound does Raindrop #{n} make?")
     let mut melody: String = String::new();
-    if (n % 3 != 0 && n % 5 != 0 && n % 7 != 0)
+    if n % 3 != 0 && n % 5 != 0 && n % 7 != 0
     {
         melody = n.to_string();
     }
     else
     {
-        if (n % 3 == 0)
+        if n % 3 == 0
         {
             melody = melody + "Pling".to_string();
         }
-        if (n % 5 == 0)
+        if n % 5 == 0
         {
             melody = melody + "Pling".to_string();
         }
-        if (n % 7 == 0)
+        if n % 7 == 0
         {
             melody = melody + "Plong".to_string();
         }
