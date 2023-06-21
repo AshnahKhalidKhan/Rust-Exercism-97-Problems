7a6e7a2d6563f8791a27c1bfd8f4767bae917e49
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-21 16:03:28 +0500

    Update HighScores.rs

--- High Scores/HighScores.rs
+++ High Scores/HighScores.rs
@@ -1,26 +1,58 @@
 #[derive(Debug)]
-pub struct HighScores;
-
-impl HighScores
+pub struct HighScores<'a>
 {
-    pub fn new(scores: &[u32]) -> Self
+    scores: &'a [u32]
+}
+
+impl<'a> HighScores<'a>
+{
+    pub fn new(scores: &'a [u32]) -> Self
     {
-        unimplemented!("Construct a HighScores struct, given the scores: {scores:?}")
+        //unimplemented!("Construct a HighScores struct, given the scores: {scores:?}")
+        Self
+        {
+            scores
+        }
     }
 
     pub fn scores(&self) -> &[u32]
     {
-        unimplemented!("Return all the scores as a slice")
+        //unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32>
     {
-        unimplemented!("Return the latest (last) score")
+        //unimplemented!("Return the latest (last) score")
+        if self.scores.len() > 0
+        {
+            return Some(self.scores[self.scores.len() - 1]);
+        }
+        else
+        {
+            return None;
+        }
     }
 
     pub fn personal_best(&self) -> Option<u32>
     {
-        unimplemented!("Return the highest score")
+        //unimplemented!("Return the highest score")
+        if self.scores.len() > 0
+        {
+            let mut top: u32 = 0;
+            for i in 0..self.scores.len()
+            {
+                if self.scores[i] > top
+                {
+                    top = self.scores[i];
+                }
+            }
+            return Some(top);
+        }
+        else
+        {
+            return None;
+        }
     }
 
     pub fn personal_top_three(&self) -> Vec<u32>
