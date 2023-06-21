1ee20ed3ee4a4b9159fec5b3fdef66fbcda3d954
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-21 15:08:04 +0500

    Update Grains.rs

--- Grains/Grains.rs
+++ Grains/Grains.rs
@@ -1,9 +1,21 @@
 pub fn square(s: u32) -> u64
 {
     //unimplemented!("grains of rice on square {s}");
+    if s > 64 || s < 1
+    {
+        panic!("Square must be between 1 and 64");
+    }
+    let grain: u64 = 2;
+    grain.pow(s - 1)
 }
 
 pub fn total() -> u64
 {
     //unimplemented!();
+    let mut totalFortune: u64 = 0;
+    for s in 1..=64
+    {
+        totalFortune = totalFortune + square(s);
+    }
+    totalFortune
 }
