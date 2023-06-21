822644b267bc59eb7b95c50d50a022dfd1de1783
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-22 00:53:32 +0500

    Update DiffieHellman.rs

--- Diffie-Hellman/DiffieHellman.rs
+++ Diffie-Hellman/DiffieHellman.rs
@@ -19,9 +19,18 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64
     //unimplemented!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
     println!("g: {}  a: {}  p: {}      ({}*{})%{}={}",g,a,p,g,a,p,(g*a) % p);
     //((g % p)*(a % p)) % p
-    g.pow(a as u32) % p
+    // g.pow(a as u32) % p
     //((g % p)*a) % p
     // ((((1/g).pow(a as u32)) as f64).powi(-1) % (p as f64)) as u64
+    let mut n = 0;
+    let mut answer: u128 = g as u128;
+    while n < a
+    {
+        answer = answer * g  as u128;
+        answer = answer % p  as u128;
+        n = n + 1;
+    }
+    (answer % p  as u128) as u64
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
@@ -29,7 +38,25 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
     //unimplemented!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
     println!("b_pub: {}  a: {}  p: {}      ({}*{})%{}={}",b_pub,a,p,b_pub,a,p,(b_pub*a) % p);
     //((b_pub % p)*(a % p)) % p
-    b_pub.pow(a as u32) % p
+    //b_pub.pow(a as u32) % p
     //((b_pub % p)*a) % p
     //((((1/b_pub).pow(a as u32)) as f64).powi(-1) % (p as f64)) as u64
+    // let mut n = 0;
+    // let mut answer = b_pub;
+    // while n < a
+    // {
+    //     answer = answer * b_pub;
+    //     answer = answer % p;
+    //     n = n + 1;
+    // }
+    // answer % p
+    let mut n = 0;
+    let mut answer: u128 = b_pub as u128;
+    while n < a
+    {
+        answer = answer * b_pub  as u128;
+        answer = answer % p  as u128;
+        n = n + 1;
+    }
+    (answer % p  as u128) as u64
 }
