b4338ff039ba93b393f263559c53f04071ea81d1
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-22 23:15:34 +0500

    Update DiffieHellman.rs

--- Diffie-Hellman/DiffieHellman.rs
+++ Diffie-Hellman/DiffieHellman.rs
@@ -81,15 +81,65 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
 {
     //unimplemented!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
-    b_pub.pow(a as u32) % p
-    // let mut n: u128 = 0;
-    // let mut answer: u128 = 1;
-    // while n < a as u128
-    // {
-    //     answer =  (answer * b_pub as u128) % p as u128;
-    //     println!("{}", answer);
-    //     n = n + 1;
-    // }
-    // answer as u64
-    //public_key(p, b_pub, a)
+        if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
+    {
+        let mut answer: u64 = 1;
+        let mut n: u64 = 0;
+        let mut theNumberTwo: u64 = 2;
+        //println!("g={}, a={}, p={}", g, a, p);
+        //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
+        while n < a
+        {
+            println!("n={}", n);
+            if n == 0
+            {
+                println!("multiplying by {}", b_pub);
+                answer = (answer * b_pub) % p;
+                n = n + 1;
+            }
+            else
+            {
+                println!("squaring");
+                answer = (answer * answer) % p;
+                n = n + n;
+            }
+            //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
+        }
+        return answer;
+    }
+    else
+    {
+        let closestPowerOfTwoToGivenA: u64 = (a as f64).log2().floor() as u64;
+        println!("closest power of 2 to a{} is {}", a, closestPowerOfTwoToGivenA);
+        let mut answer: u64 = 1;
+        let mut n: u64 = 0;
+        let mut theNumberTwo: u64 = 2;
+        //println!("g={}, a={}, p={}", g, a, p);
+        //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
+        while n < closestPowerOfTwoToGivenA
+        {
+            println!("n={}", n);
+            if n == 0
+            {
+                println!("squaring");
+                answer = (answer * answer) % p;
+                n = n + n;
+            }
+            else
+            {
+                println!("multiplying by {}", b_pub);
+                answer = (answer * b_pub) % p;
+                n = n + 1;
+            }
+            //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
+        }
+        while n < a
+        {
+            answer = (answer * b_pub) % p;
+            n = n + 1;
+        }
+        return answer;
+    }
+    //b_pub.pow(a as u32) % p
+
 }
