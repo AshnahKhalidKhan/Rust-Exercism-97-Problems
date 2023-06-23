25e63e2263060e1ec7a0371f7196a4accb92553a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-23 11:51:33 +0500

    Update DiffieHellman.rs

--- Diffie-Hellman/DiffieHellman.rs
+++ Diffie-Hellman/DiffieHellman.rs
@@ -17,91 +17,102 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64
     //unimplemented!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
     // g.pow(a as u32) % p
     println!("g={}, a={}, p={}", g, a, p);
-  
-    if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
-    {
-        let mut answer: u64 = 1;
-        let mut n: u64 = 0;
+    // if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
+    // {
+    //     let mut answer: u64 = 1;
+    //     let mut n: u64 = 0;
+    //     let mut theNumberTwo: u64 = 2;
+    //     while n < a
+    //     {
+    //         if n == 0
+    //         {
+    //             answer = (answer * g) % p;
+    //             n = n + 1;
+    //         }
+    //         else
+    //         {
+    //             answer = (answer * answer) % p;
+    //             n = n + n;
+    //         }
+    //     }
+    //     return answer;
+    // }
+    // else
+    // {
         let mut theNumberTwo: u64 = 2;
-        while n < a
-        {
-            println!("n={}", n);
-            if n == 0
-            {
-                answer = (answer * g) % p;
-                n = n + 1;
-            }
-            else
-            {
-                answer = (answer * answer) % p;
-                n = n + n;
-            }
-            //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
-        }
-        return answer;
-    }
-    else
-    {
-        let closestPowerOfTwoToGivenA: u64 = (a as f64).log2().floor() as u64;
+        let closestPowerOfTwoToGivenA: u64 = theNumberTwo.pow((a as f64).log2().floor() as u32) as u64;
         println!("closest power of 2 to a{} is {}", a, closestPowerOfTwoToGivenA);
         let mut answer: u64 = 1;
         let mut n: u64 = 0;
         let mut theNumberTwo: u64 = 2;
         while n < closestPowerOfTwoToGivenA
         {
-            if theNumberTwo.pow(n as u32) % 2 == 0
-            {
-                answer = (answer * answer) % p;
-                n = n + n;
-            }
-            else
+            println!("n={}",n);
+            if n == 0
             {
                 answer = (answer * g) % p;
                 n = n + 1;
             }
+            else
+            {
+                answer = (answer * answer) % p;
+                n = n + n;
+            }
         }
-        while n < a
+        println!("n={}",n);
+        let mut theOtherNumber: u64 = 1;
+        while n < a && closestPowerOfTwoToGivenA != a
         {
-            answer = (answer * g) % p;
-            n = n + 1;
+            println!("n={}",n);
+            // if n == 0
+            // {
+                theOtherNumber = (theOtherNumber * g) % p;
+                n = n + 1;
+            // }
+            // else
+            // {
+            //     theOtherNumber = (theOtherNumber * theOtherNumber) % p;
+            //     n = n + n;
+            // }
         }
-        return answer;
-    }
+        answer = (answer * theOtherNumber) % p;
+        answer
+        // return answer;
+    // }
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
 {
     //unimplemented!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
     //b_pub.pow(a as u32) % p
-    println!("b_pub={}, a={}, p={}", b_pub, a, p);
-    if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
-    {
-        let mut answer: u64 = 1;
-        let mut n: u64 = 0;
+    // println!("b_pub={}, a={}, p={}", b_pub, a, p);
+    // if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
+    // {
+    //     let mut answer: u64 = 1;
+    //     let mut n: u64 = 0;
+    //     let mut theNumberTwo: u64 = 2;
+    //     while n < a
+    //     {
+    //         if n == 0
+    //         {
+    //             answer = (answer * b_pub) % p;
+    //             n = n + 1;
+    //         }
+    //         else
+    //         {
+    //             answer = (answer * answer) % p;
+    //             n = n + n;
+    //         }
+    //     }
+    //     return answer;
+    // }
+    // else
+    // {
         let mut theNumberTwo: u64 = 2;
-        while n < a
-        {
-            println!("n={}", n);
-            if n == 0
-            {
-                answer = (answer * b_pub) % p;
-                n = n + 1;
-            }
-            else
-            {
-                answer = (answer * answer) % p;
-                n = n + n;
-            }
-        }
-        return answer;
-    }
-    else
-    {
-        let closestPowerOfTwoToGivenA: u64 = (a as f64).log2().floor() as u64;
+        let closestPowerOfTwoToGivenA: u64 = theNumberTwo.pow((a as f64).log2().floor() as u32) as u64;
         println!("closest power of 2 to a{} is {}", a, closestPowerOfTwoToGivenA);
         let mut answer: u128 = 1;
         let mut n: u64 = 0;
-        let mut theNumberTwo: u64 = 2;
         while n < closestPowerOfTwoToGivenA
         {
             if n == 0
@@ -116,12 +127,22 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
             }
         }
         let mut theOtherNumber: u128 = 1;
-        while n < a
+        while n < a && closestPowerOfTwoToGivenA != a
         {
-            theOtherNumber = (theOtherNumber * b_pub as u128) % p as u128;
-            n = n + 1;
+            // if n == 0
+            // {
+            //     println!("multiplying by {}", b_pub);
+                theOtherNumber = (theOtherNumber * b_pub as u128) % p as u128;
+                n = n + 1;
+            // }
+            // else
+            // {
+            //     println!("squaring other number");
+            //     theOtherNumber = (theOtherNumber * theOtherNumber) % p as u128;
+            //     n = n + n;
+            // }
         }
         answer = (answer * theOtherNumber) % p as u128;
         return answer as u64;
-    }
+    // }
 }
