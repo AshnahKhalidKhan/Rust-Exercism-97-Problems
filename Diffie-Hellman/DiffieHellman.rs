1fcc212e50f8e1f62a57c8c4305459e0c8fb992a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-23 11:13:49 +0500

    Update DiffieHellman.rs

--- Diffie-Hellman/DiffieHellman.rs
+++ Diffie-Hellman/DiffieHellman.rs
@@ -16,26 +16,23 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64
 {
     //unimplemented!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
     // g.pow(a as u32) % p
-    //h
+    println!("g={}, a={}, p={}", g, a, p);
+  
     if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
     {
         let mut answer: u64 = 1;
         let mut n: u64 = 0;
         let mut theNumberTwo: u64 = 2;
-        println!("g={}, a={}, p={}", g, a, p);
-        println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         while n < a
         {
             println!("n={}", n);
             if n == 0
             {
-                println!("multiplying by {}", g);
                 answer = (answer * g) % p;
                 n = n + 1;
             }
             else
             {
-                println!("squaring");
                 answer = (answer * answer) % p;
                 n = n + n;
             }
@@ -50,24 +47,18 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64
         let mut answer: u64 = 1;
         let mut n: u64 = 0;
         let mut theNumberTwo: u64 = 2;
-        println!("g={}, a={}, p={}", g, a, p);
-        //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         while n < closestPowerOfTwoToGivenA
         {
-            println!("n={}", n);
             if theNumberTwo.pow(n as u32) % 2 == 0
             {
-                println!("squaring");
                 answer = (answer * answer) % p;
                 n = n + n;
             }
             else
             {
-                println!("multiplying by {}", g);
                 answer = (answer * g) % p;
                 n = n + 1;
             }
-            //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         }
         while n < a
         {
@@ -81,29 +72,26 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
 {
     //unimplemented!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
-        if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
+    //b_pub.pow(a as u32) % p
+    println!("b_pub={}, a={}, p={}", b_pub, a, p);
+    if (a as f64).log2().ceil() == (a as f64).log2() && (a as f64).log2().floor() == (a as f64).log2() 
     {
         let mut answer: u64 = 1;
         let mut n: u64 = 0;
         let mut theNumberTwo: u64 = 2;
-        //println!("g={}, a={}, p={}", g, a, p);
-        //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         while n < a
         {
             println!("n={}", n);
             if n == 0
             {
-                println!("multiplying by {}", b_pub);
                 answer = (answer * b_pub) % p;
                 n = n + 1;
             }
             else
             {
-                println!("squaring");
                 answer = (answer * answer) % p;
                 n = n + n;
             }
-            //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         }
         return answer;
     }
@@ -114,47 +102,26 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64
         let mut answer: u128 = 1;
         let mut n: u64 = 0;
         let mut theNumberTwo: u64 = 2;
-        println!("g={}, a={}, p={}", b_pub, a, p);
-        //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         while n < closestPowerOfTwoToGivenA
         {
-            println!("n={} answer={}", n, answer);
             if n == 0
             {
-                println!("multiplying by {}", b_pub);
                 answer = (answer * b_pub as u128) % p as u128;
                 n = n + 1;
             }
             else
             {
-                println!("squaring");
                 answer = (answer * answer) % p as u128;
                 n = n + n;
             }
-            //println!("n={}, 2^n={}, answer={}", n, theNumberTwo.pow(n as u32), answer);
         }
-    println!("Am out");
-        // while n < a
-        // {
-        //     answer = (answer * b_pub) % p;
-        //     n = n + 1;
-        // }
-        // let mut x: u128 = b_pub.pow((a-n) as u32) as u128;
-        // x = x % p as u128;
-        // println!("Is literally this the problem");
-        // answer = answer*x;
-        // println!("or this");
         let mut theOtherNumber: u128 = 1;
         while n < a
         {
-            println!("n={}", n);
-                println!("multiplying by {}", b_pub);
-                theOtherNumber = (theOtherNumber * b_pub as u128) % p as u128;
-                n = n + 1;
+            theOtherNumber = (theOtherNumber * b_pub as u128) % p as u128;
+            n = n + 1;
         }
-    answer = (answer*theOtherNumber) % p as u128;
+        answer = (answer * theOtherNumber) % p as u128;
         return answer as u64;
     }
-    //b_pub.pow(a as u32) % p
-
 }
