c5d9990c8c29a550cc1f118c31beb3d619ab7245
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-25 00:00:26 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -8,35 +8,38 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
     let mut wordLowerCase: Vec<String> = word.chars().map(|c| c.to_lowercase().to_string()).collect();
     let mut wordRearranged: Vec<String> = wordLowerCase.clone();
     wordRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+    println!("wordLowerCase: {:?}", wordLowerCase);
     println!("wordRearranged: {:?}", wordRearranged);
     for i in possible_anagrams.iter()
     {
             if i.len() == word.len()
             {
-                let mut allLowerCase: Vec<String> = word.chars().map(|c| c.to_lowercase().to_string()).collect();
+                let mut allLowerCase: Vec<String> = i.chars().map(|c| c.to_lowercase().to_string()).collect();
             
-            let mut sameWord: bool = true;
-            for n in 0..wordLowerCase.len()
-            {
-                if wordLowerCase[n] != allLowerCase[n]
-                {
-                    sameWord = false;
-                    break;
-                }
-            }
-            if sameWord == false
+    //         let mut sameWord: bool = true;
+    //         for n in 0..wordLowerCase.len()
+    //         {
+    //             if wordLowerCase[n] != allLowerCase[n]
+    //             {
+    //                 sameWord = false;
+    //                 break;
+    //             }
+    //         }
+            println!("allLowerCase: {:?}", allLowerCase);
+            if wordLowerCase != allLowerCase//sameWord == false
             {
     allLowerCase.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
-                let mut sameArrangement: bool = true;
-                for n in 0..wordRearranged.len()
-                {
-                    if wordRearranged[n] != allLowerCase[n]
-                    {
-                        sameArrangement = false;
-                        break;
-                    }
-                }
-                if sameArrangement == true
+                println!("allLowerCaseRearranged: {:?}", allLowerCase);
+    //             let mut sameArrangement: bool = true;
+    //             for n in 0..wordRearranged.len()
+    //             {
+    //                 if wordRearranged[n] != allLowerCase[n]
+    //                 {
+    //                     sameArrangement = false;
+    //                     break;
+    //                 }
+    //             }
+                if wordRearranged == allLowerCase//sameArrangement == true
                 {
                     Anagrams.insert(i);
                 }
