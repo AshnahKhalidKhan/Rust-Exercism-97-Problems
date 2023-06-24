6caac2b55d47465f86c999e74a8ed6c3fe70136c
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 23:46:09 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -11,13 +11,36 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
     println!("wordRearranged: {:?}", wordRearranged);
     for i in possible_anagrams.iter()
     {
-            let mut allLowerCase: Vec<ToLowercase> = word.chars().map(|c| c.to_lowercase()).collect();
-    let mut lowerCaseRearranged: Vec<ToLowercase> = wordLowerCase.clone();
-    lowerCaseRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(*nextLetter).unwrap());
-        println!("lowerCaseRearranged: {:?}", lowerCaseRearranged);
-        if wordLowerCase.iter() != allLowerCase.iter() && lowerCaseRearranged.iter() == wordRearranged.iter()
-        {
-            Anagrams.insert(i);
+            if i.len() == word.len()
+            {
+                let mut allLowerCase: Vec<ToLowercase> = word.chars().map(|c| c.to_lowercase()).collect();
+            
+            let mut sameWord: bool = true;
+            for n in 0..wordLowerCase.len()
+            {
+                if wordLowerCase[n] != allLowerCase[n]
+                {
+                    sameWord = false;
+                    break;
+                }
+            }
+            if sameWord == false
+            {
+                allLowerCase.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(*nextLetter).unwrap());
+                let mut sameArrangement: bool = true;
+                for n in 0..wordRearranged.len()
+                {
+                    if wordRearranged[n] != allLowerCase[n]
+                    {
+                        sameArrangement = false;
+                        break;
+                    }
+                }
+                if sameArrangement == true
+                {
+                    Anagrams.insert(i);
+                }
+            }
         }
     }
     Anagrams
