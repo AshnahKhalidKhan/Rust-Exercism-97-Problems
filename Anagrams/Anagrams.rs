012c93a73547638edf4144ec9229e7ad4ac01c66
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 22:52:31 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -6,6 +6,7 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a s
     for i in possible_anagrams.iter()
     {
         let mut allLowerCase: Vec<char> = i.chars().map(|c| c.to_ascii_lowercase()).collect();
+        allLowerCase.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
     }
     HashSet::new()
 }
