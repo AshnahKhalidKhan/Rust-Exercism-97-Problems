76f0420d73473f4ed0f41707f1f594cb8c2b2fea
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 23:00:43 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -4,10 +4,16 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a s
 {
     //unimplemented!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
     let mut Anagrams: HashSet<&[&str]> = HashSet::new();
+    let mut wordRearranged: Vec<char> = word.chars().map(|c| c.to_ascii_lowercase()).collect();
+    wordRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
     for i in possible_anagrams.iter()
     {
         let mut allLowerCase: Vec<char> = i.chars().map(|c| c.to_ascii_lowercase()).collect();
-        allLowerCase.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+        allLowerCase.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+        if allLowerCase == wordRearranged
+        {
+            Anagrams.insert(i);
+        }
     }
-    HashSet::new()
+    Anagrams
 }
