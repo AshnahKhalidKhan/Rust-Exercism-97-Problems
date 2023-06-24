020a5d165058d2ef911f9c4f515d8ac9b17bc03a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 23:18:19 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -4,18 +4,17 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
 {
     //unimplemented!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
     let mut Anagrams: HashSet<&str> = HashSet::new();
-    let mut wordRearranged: Vec<char> = word.chars().map(|c| c.to_ascii_lowercase()).collect();
+    let mut wordLowerCase: Vec<char> = word.chars().map(|c| c.to_ascii_lowercase()).collect();
+    let mut wordRearranged: Vec<char> = wordLowerCase.clone();
     wordRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
     for i in possible_anagrams.iter()
     {
         let mut allLowerCase: Vec<char> = i.chars().map(|c| c.to_ascii_lowercase()).collect();
-        if word.chars().map(|c| c.to_ascii_lowercase()).collect() != allLowerCase
+        let mut lowerCaseRearranged: Vec<char> = allLowerCase.clone();
+        lowerCaseRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+        if wordLowerCase != allLowerCase && allLowerCase == wordRearranged
         {
-            allLowerCase.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
-            if allLowerCase == wordRearranged
-            {
-                Anagrams.insert(i);
-            }
+            Anagrams.insert(i);
         }
     }
     Anagrams
