fb41e4818c3b28dba5b01010d148721b8fcdad95
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 22:57:17 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -3,6 +3,7 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str>
 {
     //unimplemented!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+    let mut Anagrams: HashSet<&[&str]> = HashSet::new();
     for i in possible_anagrams.iter()
     {
         let mut allLowerCase: Vec<char> = i.chars().map(|c| c.to_ascii_lowercase()).collect();
