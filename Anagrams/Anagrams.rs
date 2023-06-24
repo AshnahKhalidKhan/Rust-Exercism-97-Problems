7bed1d206d543eadb650a5f2a87c431b7600eea6
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 23:37:41 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -7,15 +7,15 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
     let mut Anagrams: HashSet<&str> = HashSet::new();
     let mut wordLowerCase: Vec<ToLowercase> = word.chars().map(|c| c.to_lowercase()).collect();
     let mut wordRearranged: Vec<ToLowercase> = wordLowerCase.clone();
-    wordRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+    wordRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(*nextLetter).unwrap());
     println!("wordRearranged: {:?}", wordRearranged);
     for i in possible_anagrams.iter()
     {
             let mut allLowerCase: Vec<ToLowercase> = word.chars().map(|c| c.to_lowercase()).collect();
     let mut lowerCaseRearranged: Vec<ToLowercase> = wordLowerCase.clone();
-    lowerCaseRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+    lowerCaseRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(*nextLetter).unwrap());
         println!("lowerCaseRearranged: {:?}", lowerCaseRearranged);
-        if wordLowerCase != allLowerCase && lowerCaseRearranged == wordRearranged
+        if wordLowerCase.iter() != allLowerCase.iter() && lowerCaseRearranged.iter() == wordRearranged.iter()
         {
             Anagrams.insert(i);
         }
