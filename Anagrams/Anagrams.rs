da2ba939617809cc9b2dc8195d254711ca79a715
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-24 23:31:41 +0500

    Update Anagrams.rs

--- Anagrams/Anagrams.rs
+++ Anagrams/Anagrams.rs
@@ -7,11 +7,13 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
     let mut wordLowerCase: Vec<char> = word.chars().map(|c| c.to_ascii_lowercase()).collect();
     let mut wordRearranged: Vec<char> = wordLowerCase.clone();
     wordRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+    println!("wordRearranged: {:?}", wordRearranged);
     for i in possible_anagrams.iter()
     {
         let mut allLowerCase: Vec<char> = i.chars().map(|c| c.to_ascii_lowercase()).collect();
         let mut lowerCaseRearranged: Vec<char> = allLowerCase.clone();
         lowerCaseRearranged.sort_unstable_by(|firstLetter, nextLetter| firstLetter.partial_cmp(nextLetter).unwrap());
+        println!("lowerCaseRearranged: {:?}", lowerCaseRearranged);
         if wordLowerCase != allLowerCase && lowerCaseRearranged == wordRearranged
         {
             Anagrams.insert(i);
