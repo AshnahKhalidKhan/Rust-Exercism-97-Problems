6a206b285a1a8540dddc791921d7f3fa3c01217a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-25 13:04:33 +0500

    Update SpaceAge.rs

--- Space Age/SpaceAge.rs
+++ Space Age/SpaceAge.rs
@@ -2,30 +2,31 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration
-{
-    seconds: u64
-}
+pub struct Duration;
+// {
+//     seconds: u64
+// }
 
 impl From<u64> for Duration
 {
     fn from(s: u64) -> Self
     {
         //unimplemented!("s, measured in seconds: {s}")
-        Self
-        {
-            seconds: s
-        }
+        // Self
+        // {
+        //     seconds: s
+        // }
+        s
     }
 }
 
 pub trait Planet
 {
-    fn years_during(d: &Duration) -> f64
-    {
-        //unimplemented!("convert a duration ({d:?}) to the number of years on this planet for that duration");
-        d.seconds as f64
-    }
+    fn years_during(d: &Duration) -> f64;
+    // {
+    //     //unimplemented!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+    //     Planet::years_during(d)
+    // }
 }
 
 pub struct Mercury;
@@ -37,11 +38,74 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Mercury, d)
+    }
+}
+
+impl Planet for Venus
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Venus, d)
+    }
+}
+
+impl Planet for Earth
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Earth, d)
+    }
+}
+
+impl Planet for Mars
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Mars, d)
+    }
+}
+
+impl Planet for Jupiter
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Jupiter, d)
+    }
+}
+
+impl Planet for Saturn
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Saturn, d)
+    }
+}
+
+impl Planet for Uranus
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Uranus, d)
+    }
+}
+
+impl Planet for Mars
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Mars, d)
+    }
+}
+
+impl Planet for Neptune
+{
+    fn years_during(d: &Duration) -> f64
+    {
+        spaceAge!(Neptune, d)
+    }
+}
