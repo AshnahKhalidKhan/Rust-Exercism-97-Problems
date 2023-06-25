69cd4ea0d497af659fe43d5d41745de0d885ddc5
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-25 23:53:01 +0500

    Update Minesweeper.rs

--- Minesweeper/Minesweeper.rs
+++ Minesweeper/Minesweeper.rs
@@ -2,4 +2,5 @@ pub fn annotate(minefield: &[&str]) -> Vec<String>
 {
     //unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
     let mut numbers: Vec<String> = Vec::new();
+    numbers
 }
