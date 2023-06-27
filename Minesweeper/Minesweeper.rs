c403fd3beb09e05fa37ac9ff335ae6b047b7ddec
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-27 23:51:24 +0500

    Update Minesweeper.rs

--- Minesweeper/Minesweeper.rs
+++ Minesweeper/Minesweeper.rs
@@ -137,19 +137,4 @@ pub fn annotate(minefield: &[&str]) -> Vec<String>
         
     }
     finalnumber
-    
-    //numbers
-    /*
-         0  1  2  3  4
-         5  6  7  8  9
-        10 11 12 13 14
-        15 16 17 18 19
-        20 21 22 23 24
-
-        0  1  2  3  4
-        0  1  2  3  4
-        0  1  2  3  4
-        0  1  2  3  4
-        0  1  2  3  4
-    */
 }
