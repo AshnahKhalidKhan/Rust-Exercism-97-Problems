a55648455c772fff833169ec70d7e16d4e73f87a
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-28 15:55:15 +0500

    Update Acronym.rs

--- Acronym/Acronym.rs
+++ Acronym/Acronym.rs
@@ -1,10 +1,13 @@
 pub fn abbreviate(phrase: &str) -> String
 {
     //unimplemented!("Given the phrase '{phrase}', return its acronym");
+    println!("phrase: {}", phrase);
     let mut acronym: String = String::new();
+    println!("phrase.split_whitespace(): {:?}", phrase.split_whitespace());
     for word in phrase.split_whitespace()
     {
-        acronym = acronym + word.chars().next();
+        acronym = acronym + &word.chars().next().unwrap().to_string();
     }
+    println!("acronym: {}", acronym);
     acronym
 }
