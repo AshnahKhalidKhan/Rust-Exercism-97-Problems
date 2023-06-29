669eaca33ea10bc186ffbed606523df2ec5ae2ef
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-29 13:36:39 +0500

    Update Acronym.rs

--- Acronym/Acronym.rs
+++ Acronym/Acronym.rs
@@ -11,16 +11,23 @@ pub fn abbreviate(phrase: &str) -> String
 
     for i in words
     {
-        // let characters: Vec<char> = i.chars().map(|c: char| if c.is_uppercase() == true
-        // {c}
-        //                                          else
-        // {continue}).collect();
-        // println!("characters: {:?}", characters);
-        // let mut nextCharacterAdd: bool = true;
-        // for c in 0..characters.len()
-        // {
-        //     if c == 0 || c.is_alphabetic() == false || 
-        // }
+        let characters: Vec<char> = i.chars().collect();
+        println!("characters: {:?}", characters);
+        
+        let mut nextCharacterAdd: bool = true;
+        for c in 0..characters.len()
+        {
+            match (i, c, character[c].is_alphabetic(), nextCharacterAdd, character[c].is_uppercase(), c+1 < characters.len())
+            {
+                (_, 0, true) =>
+                {
+                    acronym = acronym + &(characters[c].to_uppercase()).to_string();
+                    nextCharacterAdd = false;
+                },
+                (_, _, _) => println!("ua")
+            };
+        }
+                
         // if i.chars().map(|c| if c.is_alphabetic() == true {c.to_ascii_uppercase()}).collect() == i.chars().map(|c| if c.is_alphabetic() == true {c}).collect()
         // {
         //     let firstCharacter: char = i.chars().next().unwrap().to_ascii_uppercase();
