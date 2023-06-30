a1f2c71298b1554728acc96ea9ef5b53406fff26
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-06-30 16:55:25 +0500

    Update LuhnForm.rs

--- Luhn Form/LuhnForm.rs
+++ Luhn Form/LuhnForm.rs
@@ -21,67 +21,67 @@ impl<'a> From<&'a str> for Luhn
     }
 }
 
-/// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool
-{
-    //unimplemented!("Is the Luhn checksum for {code} valid?");
+// /// Check a Luhn checksum.
+// pub fn is_valid(code: &str) -> bool
+// {
+//     //unimplemented!("Is the Luhn checksum for {code} valid?");
     
-    /*
-        Remove whitespaces
-        Remove non-digit characters
-        Check if length < 2
-        Reverse array
-        Double every second digit
-        Minus 9 from all doubled digits greater than 9
-        Sum all doubled resultant digits
-        Check if sum % 10 == 0
-    */
+//     /*
+//         Remove whitespaces
+//         Remove non-digit characters
+//         Check if length < 2
+//         Reverse array
+//         Double every second digit
+//         Minus 9 from all doubled digits greater than 9
+//         Sum all doubled resultant digits
+//         Check if sum % 10 == 0
+//     */
 
-    println!("Original: {}", code);
+//     println!("Original: {}", code);
     
-    let mut onlyDigits: Vec<i128> = Vec::new();
-    for c in code.chars()
-    {
-        match (c.is_ascii_digit(), c.is_whitespace())
-        {
-            (false, false) => return false,
-            (true, _) => onlyDigits.push(c.to_digit(10).unwrap() as i128),
-            (_, true) => continue,
-        };
-    }
+//     let mut onlyDigits: Vec<i128> = Vec::new();
+//     for c in code.chars()
+//     {
+//         match (c.is_ascii_digit(), c.is_whitespace())
+//         {
+//             (false, false) => return false,
+//             (true, _) => onlyDigits.push(c.to_digit(10).unwrap() as i128),
+//             (_, true) => continue,
+//         };
+//     }
 
-    println!("Only digits: {:?}", onlyDigits);
+//     println!("Only digits: {:?}", onlyDigits);
     
-    if onlyDigits.len() < 2
-    {
-        false
-    }
-    else
-    {
-        onlyDigits.reverse();
+//     if onlyDigits.len() < 2
+//     {
+//         false
+//     }
+//     else
+//     {
+//         onlyDigits.reverse();
 
-        println!("Only digits reversed: {:?}", onlyDigits);
+//         println!("Only digits reversed: {:?}", onlyDigits);
         
-        for i in 0..onlyDigits.len()
-        {
-            onlyDigits[i] = match (i % 2 == 1, onlyDigits[i]*2)
-            {
-                (true, x) if x > 9 => x - 9,
-                (true, x) => x,
-                _ => continue
-            };
-        }
+//         for i in 0..onlyDigits.len()
+//         {
+//             onlyDigits[i] = match (i % 2 == 1, onlyDigits[i]*2)
+//             {
+//                 (true, x) if x > 9 => x - 9,
+//                 (true, x) => x,
+//                 _ => continue
+//             };
+//         }
     
-        println!("Only digits after doubling & -9s: {:?}", onlyDigits);
+//         println!("Only digits after doubling & -9s: {:?}", onlyDigits);
 
-        let sum: i128 = onlyDigits.iter().sum();
+//         let sum: i128 = onlyDigits.iter().sum();
 
-        println!("sum: {}", sum);
+//         println!("sum: {}", sum);
         
-        match sum % 10
-        {
-            0 => true,
-            _ => false
-        }
-    }
-}
+//         match sum % 10
+//         {
+//             0 => true,
+//             _ => false
+//         }
+//     }
+// }
