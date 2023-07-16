f88cfba8863468930521f3eb5169b5ed35b51795
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-07-16 10:17:34 +0500

    Update Allergies.rs

--- Allergies/Allergies.rs
+++ Allergies/Allergies.rs
@@ -23,12 +23,63 @@ impl Allergies
         //unimplemented!("Given the '{score}' score, construct a new Allergies struct.");
         let mut remainingScore: u32 = score;
         let mut allergens: Vec<Allergen> = Vec::new();
-        if (remainingSc)
+        if (remainingScore >= 128)
+        {
+            allergens.push(Allergen::Cats);
+            remainingScore = remainingScore - 128;
+        }
+        if (remainingScore >= 64)
+        {
+            allergens.push(Allergen::Pollen);
+            remainingScore = remainingScore - 64;
+        }
+        if (remainingScore >= 32)
+        {
+            allergens.push(Allergen::Chocolate);
+            remainingScore = remainingScore - 32;
+        }
+        if (remainingScore >= 16)
+        {
+            allergens.push(Allergen::Tomatoes);
+            remainingScore = remainingScore - 16;
+        }
+        if (remainingScore >= 8)
+        {
+            allergens.push(Allergen::Strawberries);
+            remainingScore = remainingScore - 8;
+        }
+        if (remainingScore >= 4)
+        {
+            allergens.push(Allergen::Shellfish);
+            remainingScore = remainingScore - 4;
+        }
+        if (remainingScore >= 2)
+        {
+            allergens.push(Allergen::Peanuts);
+            remainingScore = remainingScore - 2;
+        }
+        if (remainingScore >= 1)
+        {
+            allergens.push(Allergen::Eggs);
+            remainingScore = remainingScore - 1;
+        }
+        Self
+        {
+            allergens
+        }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool
     {
-        unimplemented!("Determine if the patient is allergic to the '{allergen:?}' allergen.");
+        //unimplemented!("Determine if the patient is allergic to the '{allergen:?}' allergen.");
+        if self.allergens.contains(allergen) == true
+        {
+            true
+        }
+        else
+        {
+            false
+        }
     }
 
     pub fn allergies(&self) -> Vec<Allergen>
