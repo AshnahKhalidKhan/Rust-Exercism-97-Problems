0cce3d943537253421d2d39fd9888cb0114a2708
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-07-18 22:37:01 +0500

    Update Alphametics.rs

--- Alphametics/Alphametics.rs
+++ Alphametics/Alphametics.rs
@@ -3,4 +3,10 @@ use std::collections::HashMap;
 pub fn solve(input: &str) -> Option<HashMap<char, u8>>
 {
     unimplemented!("Solve the alphametic {input:?}")
+    //My idea is to do it using binary
+    //We _can_ do it that way
+    //Whatever number of letters we have
+    //Lets call it n
+    //Each of them can only have 9 possibilites of less than one
+    //I think I'm on to something here
 }
