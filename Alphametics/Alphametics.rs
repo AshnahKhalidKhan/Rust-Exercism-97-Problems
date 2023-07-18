ad6090601029d2d76fa98134c39431875e49ee26
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-07-18 22:32:11 +0500

    Update Alphametics.rs

--- Alphametics/Alphametics.rs
+++ Alphametics/Alphametics.rs
@@ -1,6 +1,6 @@
-
 use std::collections::HashMap;
 
-pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
+pub fn solve(input: &str) -> Option<HashMap<char, u8>>
+{
     unimplemented!("Solve the alphametic {input:?}")
 }
